8844c2c128993af96d74b5b59d3f80facd1467f0
cck <edouard@cockenpot.net>
2016-12-06 08:11:05 +0100

    Update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,11 +72,12 @@ Rails.application.configure do
       :address              => 'smtp.sendgrid.net',
       :port                 => '587',
       :authentication       => :plain,
-      :user_name            => ENV['SENDGRID MESSAGE'],
-      :password             => ENV['SENDGRID MESSAGE'],
+      :user_name            => ENV['SENDGRID_USERNAME'],
+      :password             => ENV['SENDGRID_PASSWORD'],
       :domain               => 'heroku.com',
       :enable_startlls_auto => true
   }
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
